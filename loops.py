dab722b602754e408ae6c08b4a8523a5a4d9e728
Joyce <joycel97@nycstudents.net>
2026-09-17 09:19:33 -0400

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,19 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square (100,90)
+
+turtle.done()
